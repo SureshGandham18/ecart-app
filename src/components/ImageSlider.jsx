1f14254f90ac4726322e97778343cd7df9c69fd1
Shanmukha Naga Suresh Gandham <85335126+SureshGandham18@users.noreply.github.com>
2023-03-02 22:11:43 +0530

    Update ImageSlider.jsx

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import SimpleImageSlider from "react-simple-image-slider";
 
 const images = [
-  { url: "	https://trak.in/wp-content/uploads/2018/07/Flipkart-Big-Shopping-Days-.jpg"},
-  { url: "	https://www.fusioninformatics.com/images/famous-app/flipkart-banner.jpg" },
-//   { url: "images/3.jpg" },
-//   { url: "images/4.jpg" },
-//   { url: "images/5.jpg" },
-//   { url: "images/6.jpg" },
-//   { url: "images/7.jpg" },
+  { url: "https://i.pinimg.com/736x/b6/89/96/b68996b0aeb13339740f961ada455a77.jpg" },
+  { url: "https://d1alm8p94swy6o.cloudfront.net/wp-content/uploads/2018/01/28122705/slider-img.png"},
+  { url: "https://www.abitcorp.com/images/bg-banner-service-ecommerce2.jpg" },
+  
+  { url: "https://ritikkachhot.b-cdn.net/wp-content/uploads/2022/02/695-removebg-preview.png" },
 ];
 
 const ImageSlider = () => {
@@ -20,9 +18,9 @@ const ImageSlider = () => {
         height={400}
         images={images}
         showBullets={true}
-        showNavs={true}
+        showNavs={true}   
         autoPlay={true}
-        style={{backgroundColor:'white',padding:'10px',position:'static'}}
+        style={{backgroundColor:'white',padding:'',position:'relative',display:'block',objectFit:'cover',borderRadius:"10px"}}
       />
       </center>
     </div>
